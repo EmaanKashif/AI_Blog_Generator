bfa6a3cfe80c47f3c2d86fbab9248622c2c99043
EmaanKashif <139724711+EmaanKashif@users.noreply.github.com>
2025-05-07 16:50:46 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ topic = st.text_input("Enter your blog topic:")
 if topic:
    generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
    result = generator(topic, max_length=300, num_return_sequences=1, do_sample=True, temperature=0.9)
-    with st.spinner("Generating..."):
+   with st.spinner("Generating..."):
         result = generator(topic, max_length=100, num_return_sequences=1)
         st.success("Here's your AI-generated blog snippet:")
         st.write(result[0]["generated_text"])
